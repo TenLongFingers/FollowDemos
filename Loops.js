417b75ee3034729cbe0e58e3674cebcc7115328a
Chris McKnight <tenlongfingers10@gmail.com>
2022-04-14 19:39:17 -0600

    If added

---
+++ Loops.js
@@ -0,0 +1,14 @@
+// let myStrArr = [`one`,`two`,`three`]
+
+// for(let i = 0; i< arr.length; i++)
+
+let myNumArr=[1,2,3,4,5,6]
+for(let i = 0; i<myNumArr.length; i++){
+    const isOdd = myNumArr[i] % 2 === 1;
+    if(isOdd){
+        console.log(myNumArr[i] + ` is odd`)
+    } if (myNumArr[i] % 2===0) {
+        console.log(myNumArr[i]+` is even`)
+
+    }
+}
