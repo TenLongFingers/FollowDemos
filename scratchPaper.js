3fbd11716ba9eb667efff2dc60522c8f50652f73
Chris McKnight <tenlongfingers10@gmail.com>
2022-04-15 22:31:01 -0600

    A very interesting error message that I don't have time to deal with right now

--- scratchPaper.js
+++ scratchPaper.js
@@ -16,3 +16,20 @@ for(let i = 0; i<myNumArr.length; i++){
 // testing something with arrays real quick
 
 let animalArr = [Fox, Tiger, Bat, Otter]
+let favAnimal = animalArr[0]
+let index = 1
+console.log(favAnimal)
+console.log(index)
+// error being returned:
+// C:\Users\cstra\Desktop\DevAssignments\FollowDemo\scratchPaper.js:18
+// let animalArr = [Fox, Tiger, Bat, Otter]
+//                  ^
+
+// ReferenceError: Fox is not defined
+//     at Object.<anonymous> (C:\Users\cstra\Desktop\DevAssignments\FollowDemo\scratchPaper.js:18:18)
+//     at Module._compile (node:internal/modules/cjs/loader:1103:14)
+//     at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
+//     at Module.load (node:internal/modules/cjs/loader:981:32)
+//     at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)
+//     at node:internal/main/run_main_module:17:47
